e53425d19d77ba58b3719d89c799097a90458404
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2020-10-14 17:58:20 +0200

    Post major cities
    Population > 0.5 M

--- script.R
+++ script.R
@@ -4,11 +4,11 @@ numCores <- parallel::detectCores()
 source('auxs.R')
 
 # Config
-population_threshold <- 100000
+population_threshold <- 500000
 
 # Create the dataset
 cities_es <- get_towns(c('Spain', 'Canary Islands'), population_threshold)
 city_names <- cities_es$name
 
 
-mclapply(city_names, function(city_name) plot_static_city(city_name, population_threshold = population_threshold, save_path = "./img/es/"), mc.cores = numCores - 1)
+mclapply(city_names, function(city_name) plot_static_city(city_name, population_threshold = population_threshold, save_path = "./figs/es/"), mc.cores = numCores - 1)
